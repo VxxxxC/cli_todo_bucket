586d7293e0b782a63ded38ef69cde97b4fae2652
vxxxxc <>
2025-03-22 20:31:00 +0800

    define subcommand struct

--- src/cli.rs
+++ src/cli.rs
@@ -3,10 +3,48 @@ use clap::{Args, Parser, Subcommand};
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct CliArgs {
-    /// First Args
-    pub first_args: String,
-    /// Second Args
-    pub second_args: String,
-    /// Third Args
-    pub third_args: String,
+    #[clap(subcommand)]
+    pub input_type: InputType,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum InputType {
+    /// Create, add new todo items
+    Add(AddItem),
+    /// Update the todo list
+    Update(UpdateCommand),
+    /// Check the current todo list
+    Check,
+}
+
+#[derive(Debug, Args)]
+pub struct UpdateCommand {
+    #[clap(subcommand)]
+    pub command: UpdateSubcommand,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum UpdateSubcommand {
+    /// Update todo item
+    Update(UpdateItem),
+    /// Delete todo item
+    Delete(DeleteItem),
+}
+
+#[derive(Debug, Args)]
+pub struct AddItem {
+    /// update selected item
+    pub item : String,
+}
+
+#[derive(Debug, Args)]
+pub struct UpdateItem {
+    /// update selected item
+    pub item : String,
+}
+
+#[derive(Debug, Args)]
+pub struct DeleteItem {
+    /// update selected item
+    pub item: String,
 }
